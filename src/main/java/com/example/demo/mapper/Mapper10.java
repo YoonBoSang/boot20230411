c5f0ce61a698c9649820982ead01542c807b2da1
BoSang Yoon <qhtkddl002@naver.com>
2023-05-02 14:12:48 +0900

    23-05-02

--- src/main/java/com/example/demo/mapper/Mapper10.java
+++ src/main/java/com/example/demo/mapper/Mapper10.java
@@ -5,4 +5,18 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface Mapper10 {
 
+	@Update("""
+			UPDATE Bank
+			SET money = money - 500
+			WHERE name = 'A'
+			""")
+	void minusA();
+
+	@Update("""
+			UPDATE Bank
+			SET money = money + 500
+			WHERE name = 'B'
+			""")
+	void plusB();
+
 }
