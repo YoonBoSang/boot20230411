605e9cf7c1838bf1b4f90ff6524e770c0d8e7908
BoSang Yoon <qhtkddl002@naver.com>
2023-05-09 15:33:34 +0900

    23-05-09

--- src/main/java/com/example/demo/controller/Controller33.java
+++ src/main/java/com/example/demo/controller/Controller33.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.springframework.security.access.prepost.*;
+import org.springframework.security.core.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,5 +13,24 @@ public class Controller33 {
 	public void loginForm() {
 		
 		
+	}
+	
+	@GetMapping("loginSuccess")
+	public void loginSuccess() {
+		
+	}
+	
+	@GetMapping("viewAuth")
+	public void viewAuth(Authentication authentication) {
+		System.out.println("login information");
+		
+		System.out.println(authentication);
+		System.out.println(authentication.getName());
+	}
+	
+	@GetMapping("customCheck")
+	@PreAuthorize("@securityUtil.checkBoardWriter(authentication, #id)")
+	public void customCheck(String id) {
+		System.out.println("customCheck 메소드 실행중.");
 	}
 }
